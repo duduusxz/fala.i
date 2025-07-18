285d3ebdbe0c31f13021241b6d23634057755b68
gui <guisolon.tec@gmail.com>
2025-07-18 20:21:56 -0300

    Realizando comentários mais específicos para deixar o código manutenivel e legivel

--- app.py
+++ app.py
@@ -24,27 +24,27 @@ from controller.auth_controller import auth_bp
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'uma_chave_secreta_muito_longa_e_aleatoria_para_producao_1234567890'
 app.secret_key = 'chave_secreta_segura' 
-app.register_blueprint(auth_bp) #registra rotas com blueprint, para manipular com "auth.exemplo", sem problemas
+app.register_blueprint(auth_bp) #register the routes with blueprints, example: auth.example, and he integrate with auth_controler, for not have  problems with route, others = entre outros or outros.
 
 
 
 @app.route("/")
 def index():
-    return render_template("PaginaLogin/PaginaLogin.html")
+    return render_template("PaginaLogin/PaginaLogin.html") # here he return paginalogin because its route first
 
-@app.route("/resposta", methods=["POST", "GET"])
+@app.route("/resposta", methods=["POST", "GET"]) # here configured for get methods post and get for validation information and verify with database
 def resposta():
     pergunta = None
     resposta_chatbot = None
 
     if request.method == "POST":
-        pergunta = request.form.get("pergunta")
-        if pergunta:
-            resposta_chatbot = gerar_resposta(pergunta)
+        pergunta = request.form.get("pergunta") # he to do request in names that were configured in html
+        if pergunta: #if your question is true, he to go valid your question and generate the response
+            resposta_chatbot = gerar_resposta(pergunta) # get the craete function
 
     return render_template("PaginaChatbot/PaginaChatbot.html", pergunta=pergunta, resposta=resposta_chatbot)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port) # configuration the port for start  the project because just to can function after in deploy
